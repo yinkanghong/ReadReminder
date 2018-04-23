317e2cd01034e42d6cbfee9cc399d87224829f1b
ykh <ykh@dreamgo.tech>
2018-04-23 13:32:33 +0800

    2018/04/23 修正static_url错误

--- ReadReminder/settings.py
+++ ReadReminder/settings.py
@@ -113,7 +113,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATICFILES_DIRS = (
 )
